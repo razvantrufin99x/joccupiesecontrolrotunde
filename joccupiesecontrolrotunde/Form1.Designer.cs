a1d430d143af438baa0f41ee820bb72de7ba79ba
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:44:14 +0200

    Add project files.

---
+++ joccupiesecontrolrotunde/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace joccupiesecontrolrotunde
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new controlrotund.UserControl1();
+            this.userControl12 = new controlrotund.UserControl1();
+            this.userControl13 = new controlrotund.UserControl1();
+            this.userControl14 = new controlrotund.UserControl1();
+            this.userControl15 = new controlrotund.UserControl1();
+            this.userControl16 = new controlrotund.UserControl1();
+            this.userControl17 = new controlrotund.UserControl1();
+            this.userControl18 = new controlrotund.UserControl1();
+            this.userControl19 = new controlrotund.UserControl1();
+            this.userControl110 = new controlrotund.UserControl1();
+            this.userControl111 = new controlrotund.UserControl1();
+            this.userControl112 = new controlrotund.UserControl1();
+            this.userControl113 = new controlrotund.UserControl1();
+            this.userControl114 = new controlrotund.UserControl1();
+            this.userControl115 = new controlrotund.UserControl1();
+            this.userControl116 = new controlrotund.UserControl1();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.White;
+            this.userControl11.Location = new System.Drawing.Point(12, 12);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(89, 100);
+            this.userControl11.TabIndex = 0;
+            // 
+            // userControl12
+            // 
+            this.userControl12.BackColor = System.Drawing.Color.White;
+            this.userControl12.Location = new System.Drawing.Point(107, 12);
+            this.userControl12.Name = "userControl12";
+            this.userControl12.Size = new System.Drawing.Size(89, 100);
+            this.userControl12.TabIndex = 1;
+            // 
+            // userControl13
+            // 
+            this.userControl13.BackColor = System.Drawing.Color.White;
+            this.userControl13.Location = new System.Drawing.Point(107, 118);
+            this.userControl13.Name = "userControl13";
+            this.userControl13.Size = new System.Drawing.Size(89, 100);
+            this.userControl13.TabIndex = 3;
+            // 
+            // userControl14
+            // 
+            this.userControl14.BackColor = System.Drawing.Color.White;
+            this.userControl14.Location = new System.Drawing.Point(12, 118);
+            this.userControl14.Name = "userControl14";
+            this.userControl14.Size = new System.Drawing.Size(89, 100);
+            this.userControl14.TabIndex = 2;
+            // 
+            // userControl15
+            // 
+            this.userControl15.BackColor = System.Drawing.Color.White;
+            this.userControl15.Location = new System.Drawing.Point(297, 118);
+            this.userControl15.Name = "userControl15";
+            this.userControl15.Size = new System.Drawing.Size(89, 100);
+            this.userControl15.TabIndex = 7;
+            // 
+            // userControl16
+            // 
+            this.userControl16.BackColor = System.Drawing.Color.White;
+            this.userControl16.Location = new System.Drawing.Point(202, 118);
+            this.userControl16.Name = "userControl16";
+            this.userControl16.Size = new System.Drawing.Size(89, 100);
+            this.userControl16.TabIndex = 6;
+            // 
+            // userControl17
+            // 
+            this.userControl17.BackColor = System.Drawing.Color.White;
+            this.userControl17.Location = new System.Drawing.Point(297, 12);
+            this.userControl17.Name = "userControl17";
+            this.userControl17.Size = new System.Drawing.Size(89, 100);
+            this.userControl17.TabIndex = 5;
+            // 
+            // userControl18
+            // 
+            this.userControl18.BackColor = System.Drawing.Color.White;
+            this.userControl18.Location = new System.Drawing.Point(202, 12);
+            this.userControl18.Name = "userControl18";
+            this.userControl18.Size = new System.Drawing.Size(89, 100);
+            this.userControl18.TabIndex = 4;
+            // 
+            // userControl19
+            // 
+            this.userControl19.BackColor = System.Drawing.Color.White;
+            this.userControl19.Location = new System.Drawing.Point(297, 330);
+            this.userControl19.Name = "userControl19";
+            this.userControl19.Size = new System.Drawing.Size(89, 100);
+            this.userControl19.TabIndex = 15;
+            // 
+            // userControl110
+            // 
+            this.userControl110.BackColor = System.Drawing.Color.White;
+            this.userControl110.Location = new System.Drawing.Point(202, 330);
+            this.userControl110.Name = "userControl110";
+            this.userControl110.Size = new System.Drawing.Size(89, 100);
+            this.userControl110.TabIndex = 14;
+            // 
+            // userControl111
+            // 
+            this.userControl111.BackColor = System.Drawing.Color.White;
+            this.userControl111.Location = new System.Drawing.Point(297, 224);
+            this.userControl111.Name = "userControl111";
+            this.userControl111.Size = new System.Drawing.Size(89, 100);
+            this.userControl111.TabIndex = 13;
+            // 
+            // userControl112
+            // 
+            this.userControl112.BackColor = System.Drawing.Color.White;
+            this.userControl112.Location = new System.Drawing.Point(202, 224);
+            this.userControl112.Name = "userControl112";
+            this.userControl112.Size = new System.Drawing.Size(89, 100);
+            this.userControl112.TabIndex = 12;
+            // 
+            // userControl113
+            // 
+            this.userControl113.BackColor = System.Drawing.Color.White;
+            this.userControl113.Location = new System.Drawing.Point(107, 330);
+            this.userControl113.Name = "userControl113";
+            this.userControl113.Size = new System.Drawing.Size(89, 100);
+            this.userControl113.TabIndex = 11;
+            // 
+            // userControl114
+            // 
+            this.userControl114.BackColor = System.Drawing.Color.White;
+            this.userControl114.Location = new System.Drawing.Point(12, 330);
+            this.userControl114.Name = "userControl114";
+            this.userControl114.Size = new System.Drawing.Size(89, 100);
+            this.userControl114.TabIndex = 10;
+            // 
+            // userControl115
+            // 
+            this.userControl115.BackColor = System.Drawing.Color.White;
+            this.userControl115.Location = new System.Drawing.Point(107, 224);
+            this.userControl115.Name = "userControl115";
+            this.userControl115.Size = new System.Drawing.Size(89, 100);
+            this.userControl115.TabIndex = 9;
+            // 
+            // userControl116
+            // 
+            this.userControl116.BackColor = System.Drawing.Color.White;
+            this.userControl116.Location = new System.Drawing.Point(12, 224);
+            this.userControl116.Name = "userControl116";
+            this.userControl116.Size = new System.Drawing.Size(89, 100);
+            this.userControl116.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(642, 500);
+            this.Controls.Add(this.userControl19);
+            this.Controls.Add(this.userControl110);
+            this.Controls.Add(this.userControl111);
+            this.Controls.Add(this.userControl112);
+            this.Controls.Add(this.userControl113);
+            this.Controls.Add(this.userControl114);
+            this.Controls.Add(this.userControl115);
+            this.Controls.Add(this.userControl116);
+            this.Controls.Add(this.userControl15);
+            this.Controls.Add(this.userControl16);
+            this.Controls.Add(this.userControl17);
+            this.Controls.Add(this.userControl18);
+            this.Controls.Add(this.userControl13);
+            this.Controls.Add(this.userControl14);
+            this.Controls.Add(this.userControl12);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private controlrotund.UserControl1 userControl11;
+        private controlrotund.UserControl1 userControl12;
+        private controlrotund.UserControl1 userControl13;
+        private controlrotund.UserControl1 userControl14;
+        private controlrotund.UserControl1 userControl15;
+        private controlrotund.UserControl1 userControl16;
+        private controlrotund.UserControl1 userControl17;
+        private controlrotund.UserControl1 userControl18;
+        private controlrotund.UserControl1 userControl19;
+        private controlrotund.UserControl1 userControl110;
+        private controlrotund.UserControl1 userControl111;
+        private controlrotund.UserControl1 userControl112;
+        private controlrotund.UserControl1 userControl113;
+        private controlrotund.UserControl1 userControl114;
+        private controlrotund.UserControl1 userControl115;
+        private controlrotund.UserControl1 userControl116;
+    }
+}
+
